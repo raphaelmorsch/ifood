3244d0aed12cb61eb1f5591e036af2bf1ca55c8b
Raphael Morsch <raphael@intersystem.eti.br>
2020-09-26 23:27:37 +0200

    Testes com TestContainers, Database Rider e Approval Tests

--- cadastro/src/test/java/com/github/raphaelmorsch/ifood/cadastro/NativeHelloResourceIT.java
+++ cadastro/src/test/java/com/github/raphaelmorsch/ifood/cadastro/NativeHelloResourceIT.java
@@ -3,7 +3,7 @@ package com.github.raphaelmorsch.ifood.cadastro;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeHelloResourceIT extends HelloResourceTest {
+public class NativeHelloResourceIT extends RestauranteResourceTest {
 
     // Execute the same tests but in native mode.
 }
