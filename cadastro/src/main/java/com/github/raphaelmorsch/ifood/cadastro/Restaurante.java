fbf483de2ab74408190f76c71662d5d2da262787
Raphael Morsch <raphael@intersystem.eti.br>
2020-09-26 17:55:03 +0200

    Finalizing basic nasty CRUD

--- cadastro/src/main/java/com/github/raphaelmorsch/ifood/cadastro/Restaurante.java
+++ cadastro/src/main/java/com/github/raphaelmorsch/ifood/cadastro/Restaurante.java
@@ -1,12 +1,16 @@
 package com.github.raphaelmorsch.ifood.cadastro;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -17,25 +21,25 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 @Entity
 @Table(name = "restaurante")
 public class Restaurante extends PanacheEntityBase {
-	
-	 	@Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    public Long id;
-	 	
-	 	public String proprietario;
-	 	
-	 	public String cnpj;
-	 	
-	 	public String nome;
-	 	
-	 	@CreationTimestamp
-	 	public LocalDateTime dataCriacao;
-	 	
-	 	@UpdateTimestamp
-	 	public LocalDateTime dataAtualizacao;
-	 	
-	 	@ManyToOne
-	 	public Localizacao localizacao;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
+
+	public String proprietario;
+
+	public String cnpj;
+
+	public String nome;
+
+	@CreationTimestamp
+	public LocalDateTime dataCriacao;
+
+	@UpdateTimestamp
+	public LocalDateTime dataAtualizacao;
+
+	@ManyToOne
+	public Localizacao localizacao;
 
 
 }
--- cadastro/src/main/java/com/github/raphaelmorsch/ifood/cadastro/RestauranteResource.java
+++ cadastro/src/main/java/com/github/raphaelmorsch/ifood/cadastro/RestauranteResource.java
@@ -16,8 +16,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
+
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.tags.Tags;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
@@ -27,18 +31,21 @@ import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 public class RestauranteResource {
 
 	@GET
+	@Tag(ref = "restaurante")
 	public List<PanacheEntityBase> buscar() {
 		return Restaurante.listAll();
 	}
 
 	@POST
 	@Transactional
+	@Tag(ref = "restaurante")
 	/**
 	 * 
 	 * @param dto
 	 * @param uriInfo
 	 * @return returns the response status and the URI for the resource in order to
-	 * follow the HATEOAS pattern for RESTful applications (Hypertext as Engine of Application State)
+	 *         follow the HATEOAS pattern for RESTful applications (Hypertext as
+	 *         Engine of Application State)
 	 */
 	public Response adicionar(Restaurante dto, @Context UriInfo uriInfo) {
 		dto.persist();
@@ -50,11 +57,12 @@ public class RestauranteResource {
 	@PUT
 	@Transactional
 	@Path("{id}")
+	@Tag(ref = "restaurante")
 	public void atualizar(@PathParam("id") Long id, Restaurante dto) {
 
 		Optional<Restaurante> restaurante = Restaurante.findByIdOptional(id);
 
-		if (restaurante.isEmpty()) {
+		if (!restaurante.isPresent()) {
 			throw new NotFoundException();
 		}
 
@@ -66,11 +74,91 @@ public class RestauranteResource {
 	@DELETE
 	@Transactional
 	@Path("{id}")
+	@Tag(ref = "restaurante")
 	public void remover(@PathParam("id") Long id) {
 
 		Optional<Restaurante> restaurante = Restaurante.findByIdOptional(id);
 
-		restaurante.ifPresentOrElse(Restaurante::delete, () -> {
+		if (restaurante.isPresent())
+			restaurante.get().delete();
+		else
+			throw new NotFoundException();
+
+	}
+
+	@GET
+	@Path("{id}/pratos")
+	@Tags(refs = { "prato", "restaurante" })
+	public List<Prato> buscarPratos(@PathParam("id") Long restauranteId) {
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(restauranteId);
+		if (!restauranteOp.isPresent()) {
+			throw new NotFoundException("Restaurante não localizado", Response.status(Status.NOT_FOUND).build());
+		}
+		return Prato.list("restaurante", restauranteOp.get());
+	}
+
+	@POST
+	@Transactional
+	@Path("{restauranteId}/pratos")
+	@Tags(refs = { "restaurante", "prato" })
+	public Response adicionarPrato(@PathParam("restauranteId") Long restauranteId, Prato dto,
+			@Context UriInfo uriInfo) {
+
+		Optional<Restaurante> restaurante = Restaurante.findByIdOptional(restauranteId);
+
+		restaurante.ifPresentOrElse(r -> {
+			dto.restaurante = r;
+			dto.persist();
+		}, () -> {
+			throw new NotFoundException();
+		});
+
+		UriBuilder uriBuilder = uriInfo.getAbsolutePathBuilder();
+		uriBuilder.path(Long.toString(dto.id));
+		return Response.created(uriBuilder.build()).build();
+	}
+
+	@PUT
+	@Transactional
+	@Path("{restauranteId}/pratos/{id}")
+	@Tags(refs = { "restaurante", "prato" })
+	public void atualizarPrato(@PathParam("restauranteId") Long restauranteId, @PathParam("id") Long id, Prato dto) {
+
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(restauranteId);
+
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException("Restaurante não Localizado");
+		}
+
+		Optional<Prato> prato = Prato.findByIdOptional(id);
+
+		if (prato.isEmpty()) {
+			throw new NotFoundException();
+		}
+
+		prato.get().descricao = dto.descricao;
+		prato.get().nome = dto.nome;
+		prato.get().preco = dto.preco;
+		prato.get().restaurante = restauranteOp.get();
+
+		prato.get().persist();
+	}
+
+	@DELETE
+	@Transactional
+	@Path("{restauranteId}/pratos/{id}")
+	@Tags(refs = { "restaurante", "prato" })
+	public void removerPrato(@PathParam("restauranteId") Long restauranteId, @PathParam("id") Long id) {
+
+		Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(restauranteId);
+
+		if (restauranteOp.isEmpty()) {
+			throw new NotFoundException();
+		}
+
+		Optional<Prato> pratoOp = Prato.findByIdOptional(id);
+
+		pratoOp.ifPresentOrElse(Prato::delete, () -> {
 			throw new NotFoundException();
 		});
 	}
